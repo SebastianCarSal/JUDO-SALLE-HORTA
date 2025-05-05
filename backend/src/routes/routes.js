ca2693e72a825a8f273d6cfb305618b4ef013029
SebastianCarSal <sebas.salazarl@gmail.com>
2025-05-05 20:04:28 +0200

    competidores sacados de la bbdd

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const { getJudokas } = require('../controllers/judokasController');
 const { getNoticias } = require('../controllers/noticiasController');
+const { bucket } = require('../firebase'); //
+
 
 const router = express.Router();
 
 // Rutas
 router.get('/judokas', getJudokas);
 router.get('/noticias', getNoticias);
+router.get('/fotosCompetidores', async (req, res) => {
+    try {
+      // Listar los archivos en el bucket de Firebase Storage
+      const [files] = await bucket.getFiles({ prefix: 'competidores/' }); // Cambia el prefijo según tu estructura en Firebase Storage
+  
+      // Generar URLs firmadas para cada archivo
+      const urls = await Promise.all(
+        files.map(async (file) => {
+          const [url] = await file.getSignedUrl({
+            action: 'read', 
+            expires: '03-01-2030', 
+          });
+          return { name: file.name, url }; 
+        })
+      );
+  
+      res.json(urls); 
+    } catch (error) {
+      console.error('Error al obtener las imágenes:', error.message);
+      res.status(500).json({ error: 'No se pudieron obtener las imágenes' });
+    }
+  });
+
+
 
 module.exports = router;

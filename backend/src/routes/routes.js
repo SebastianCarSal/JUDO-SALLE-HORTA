daf3499e45df503458b44c99270a1c5b3c877929
sebassalazarl <113916556+sebassalazarl@users.noreply.github.com>
2025-05-10 19:07:17 +0200

    cambio fondo

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -9,28 +9,30 @@ const router = express.Router();
 // Rutas
 router.get('/judokas', getJudokas);
 router.get('/noticias', getNoticias);
+
+
 router.get('/fotosCompetidores', async (req, res) => {
-    try {
-      // Listar los archivos en el bucket de Firebase Storage
-      const [files] = await bucket.getFiles({ prefix: 'competidores/' }); // Cambia el prefijo según tu estructura en Firebase Storage
-  
-      // Generar URLs firmadas para cada archivo
-      const urls = await Promise.all(
-        files.map(async (file) => {
-          const [url] = await file.getSignedUrl({
-            action: 'read', 
-            expires: '03-01-2030', 
-          });
-          return { name: file.name, url }; 
-        })
-      );
-  
-      res.json(urls); 
-    } catch (error) {
-      console.error('Error al obtener las imágenes:', error.message);
-      res.status(500).json({ error: 'No se pudieron obtener las imágenes' });
-    }
-  });
+  try {
+    // Listar los archivos en la carpeta "competidores" del bucket
+    const [files] = await bucket.getFiles({ prefix: 'competidores/' });
+
+    // Generar URLs firmadas para cada archivo
+    const urls = await Promise.all(
+      files.map(async (file) => {
+        const [url] = await file.getSignedUrl({
+          action: 'read', 
+          expires: '03-01-2030', 
+        });
+        return { name: file.name, url }; 
+      })
+    );
+
+    res.json(urls); 
+  } catch (error) {
+    console.error('Error al obtener las imágenes:', error.message);
+    res.status(500).json({ error: 'No se pudieron obtener las imágenes' });
+  }
+});
 
 
 
